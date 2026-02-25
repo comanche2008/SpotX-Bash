496309d7ca789c7e24c87f12f081d80ced115d48
jetfir3 <95306468+jetfir3@users.noreply.github.com>
2026-02-25 10:56:56 -0500

    Support 1.2.84.474.g9f006f58

--- spotx.sh
+++ spotx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-buildVer="1.2.84.472.g6eba3a1b"
+buildVer="1.2.84.474.g9f006f58"
 
 command -v perl >/dev/null || { echo -e "\n${red}Error:${clr} perl command not found.\nInstall perl on your system then try again.\n" >&2; exit 1; }
 
